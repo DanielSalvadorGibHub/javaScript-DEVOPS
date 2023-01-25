5b1d49b08f17594f02206e05ad000687abace204
Daniel Salvador <105977232+DanielSalvadorGibHub@users.noreply.github.com>
2023-01-24 23:30:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let numero = 9; // number literal
+let cidade = "Curitiba"; //string
+let estaAprovado = true; // boolean
+let sobrenome ; // undefined
+
+
+//propiedade
+let estado ={
+    numero: 9,
+    cidade: "Curitiba",
+    estaAprovado: true,
+    sobrenome: "Cidae fria"
+};
+
+console.log(estado);
